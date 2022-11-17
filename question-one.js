607c57dfd9bc8ac3786d7146a5b1386cc88c1138
MENTOR <odiichibueze7@gmail.com>
2022-11-17 06:56:54 +0100

    code refactor

--- question-one.js
+++ question-one.js
@@ -1,15 +1,15 @@
 //// Question One
 
-const fibonacci = (n) => {
+const fibonacci = (n) => { // O(n)
     if (n === 0 || n === 1) {
 
-        return n;
+        return n; /// O(1)
     }
-    return fibonacci(n - 2) + fibonacci(n - 1);
+    return fibonacci(n - 2) + fibonacci(n - 1); /// O(2^n)
     /// recursion
 }
 
 console.log(fibonacci(12))
-/// Time complexity : O(log n);
-/// Space complexity: 0(1)
+/// Time complexity : O(2^n);
+/// Space complexity: 0(n)
 
--- question-two.js
+++ question-two.js
@@ -1,12 +1,14 @@
 //// Question Two
 
 const missing_number = (arr) => {
-    let n = arr.length + 1;
-    let total_sum = (n * (n + 1)) / 2;
-    let arr_sum = arr.reduce(function (a, b) {return a + b;}, 0);
+    let n = arr.length + 1; // O(1)
+    let total_sum = (n * (n + 1)) / 2; // O(n)
+    let arr_sum = arr.reduce(function (a, b) {return a + b;}, 0); // O(1)
     return total_sum - arr_sum;
 }
 
+
+//
 
 console.log(missing_number([1,2,3,4,6,7,8]))
 
